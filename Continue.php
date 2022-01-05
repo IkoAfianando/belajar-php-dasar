971c872bf419bde3612734e6902e85a3add1c4cc
IkoAfianando <ikoafianando123@gmail.com>
2022-01-06 04:54:03 +0700

    Push

---
+++ Continue.php
@@ -0,0 +1,11 @@
+<?php
+
+//for($counter = 1; $counter <= 100; $counter++) {
+//    if($counter % 2 === 0) {
+//        continue;
+//    } // disini akan melanjutkan atau mengskip bilangan genap
+//    echo "Hello continue $counter" . PHP_EOL;
+//}
+
+
+
