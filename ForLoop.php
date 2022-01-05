843cbd6165e58ee862574cebfd583a50b3ed577d
IkoAfianando <ikoafianando123@gmail.com>
2022-01-06 04:40:11 +0700

    Push

---
+++ ForLoop.php
@@ -0,0 +1,12 @@
+<?php
+
+//for( ; ; ) {
+//    echo "Ini adalah For Loop" . PHP_EOL;
+//}
+
+
+
+for($counter = 1; $counter <= 10; $counter++) {
+    echo "Ini adalah For Loop ke $counter" . PHP_EOL;
+}
+
