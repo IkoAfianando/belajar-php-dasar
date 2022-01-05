80b6b266eb6dc5faf1697384a52c0e1f9ef3e459
IkoAfianando <ikoafianando123@gmail.com>
2022-01-05 17:18:55 +0700

    Push

---
+++ TipeDataNumber.php
@@ -0,0 +1,28 @@
+<?php
+
+echo "Decimal : ";
+var_dump(1234);
+
+echo "Octal : ";
+var_dump(01234);
+
+echo "Hexadecimal : ";
+var_dump(0x1A);
+
+echo "Binary : ";
+var_dump(0101);
+
+echo "Underscore in number : ";
+var_dump(1_241_23);
+
+echo "Floating Point : ";
+var_dump(1.234);
+
+echo "Floating point dengan E notation Plus : ";
+var_dump(1.7e3);
+
+echo "Floating point dengan E notation Minus : ";
+var_dump(1.7e-3);
+
+echo "Underscore dengan Floating Point : ";
+var_dump(1_234.123);
