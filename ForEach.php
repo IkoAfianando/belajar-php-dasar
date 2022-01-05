e4671c1d99b6da492f041c37e40996cbf06e1c71
IkoAfianando <ikoafianando123@gmail.com>
2022-01-06 05:20:55 +0700

    Push

---
+++ ForEach.php
@@ -0,0 +1,9 @@
+<?php
+
+$names = ["Iko", "Afianando", "Iko Afianando"];
+
+for($i = 0; $i < count($names); $i++) { // count digunakan untuk menghitung seluruh jumlah index
+    echo "Hello $names[$i]" . PHP_EOL;
+}
+
+
