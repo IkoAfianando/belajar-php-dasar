351e4b7e377cebcac40021a255ea769bc2e77e74
IkoAfianando <ikoafianando123@gmail.com>
2022-01-05 19:18:15 +0700

    Push

--- TipeDataString.php
+++ TipeDataString.php
@@ -6,3 +6,17 @@ echo "\n";
 
 echo "Name : ";
 echo "Iko\t Afianando\n";
+
+// bisa juga menggunakan text IKO sebagai contoh
+echo <<<IKO
+Selamat Belajar PHP Dasar, Sekarang kita belajar tipe data String,
+ini adalah cara ketiga untuk membuat String, bisa menggunakan
+heredoc
+
+IKO;
+
+echo <<<'IKO'
+Selamat Belajar PHP Dasar, Sekarang kita belajar tipe data String,
+ini adalah cara ketiga untuk membuat String, bisa menggunakan
+nowdoc
+IKO;
---
+++ Variable.php
@@ -0,0 +1,12 @@
+<?php
+
+$name = "Iko";
+$age = 17;
+
+echo "Name : ";
+echo $name;
+echo "\n";
+
+echo "Age : ";
+echo $age;
+echo "\n";
