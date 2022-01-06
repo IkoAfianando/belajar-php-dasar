7d17370496d9a8bc6d0c1eaa98a24e73bf1dd2c3
IkoAfianando <ikoafianando123@gmail.com>
2022-01-06 09:43:35 +0700

    Push

--- ForEach.php
+++ ForEach.php
@@ -3,7 +3,20 @@
 $names = ["Iko", "Afianando", "Iko Afianando"];
 
 for($i = 0; $i < count($names); $i++) { // count digunakan untuk menghitung seluruh jumlah index
-    echo "Hello $names[$i]" . PHP_EOL;
+    echo "Data ke $i = $names[$i]" . PHP_EOL;
 }
 
+foreach ($names as $name) {
+    echo "Data $name" . PHP_EOL;
+}
 
+$person = [
+    "first_name" => "Iko",
+    "last_name" => "Afianando",
+    "full_name" => "Iko Afianando"
+];
+
+foreach ($person as $key => $value) {
+    echo "$key : $value ". PHP_EOL;
+
+}
